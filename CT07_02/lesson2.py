ebfbf52a107739efceff5b1951fd1342fe0a7a4e
Wayne657-glitch <flamyy@outlook.com>
2025-01-18 07:02:33 +0000

    Jan 18, 2025, 7:02 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,11 +1,11 @@
 
 
-def function():
-    for i in range(21):
+
+for i in range(21):
         print(i)
     
-    for i in range(1, 31):
+for i in range(1, 31):
         print(i)
     
-    for i in range(2, 41):
+for i in range(2, 41):
         print(i)
