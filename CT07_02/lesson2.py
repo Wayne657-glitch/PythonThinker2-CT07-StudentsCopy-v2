1d88d090bc9b7109083f13b2acc1adb06da1a28f
Wayne657-glitch <flamyy@outlook.com>
2025-01-18 07:14:51 +0000

    Jan 18, 2025, 7:14 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,13 +1,1 @@
-import math
 
-def calculate_pi(n):
-    pi = 0
-    for k in range(n):
-        pi += (1 / (16 ** k)) * (
-            (4 / (8 * k + 1)) -
-            (2 / (8 * k + 4)) -
-            (1 / (8 * k + 5)) -
-            (1 / (8 * k + 6))
-        )
-    return pi
-    
