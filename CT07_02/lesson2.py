cfcfb304f82d635b75a60a58d7005032475cd31e
Wayne657-glitch <flamyy@outlook.com>
2025-01-18 07:01:47 +0000

    Jan 18, 2025, 7:01 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,5 +1,11 @@
 print("Hello from lesson 2")
 
 def function():
-    for i in range(5):
-        print(i)    
+    for i in range(21):
+        print(i)
+    
+    for i in range(1, 31):
+        print(i)
+    
+    for i in range(2, 41):
+        print(i)
