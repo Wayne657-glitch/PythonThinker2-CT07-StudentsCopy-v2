09c02cd1116c21b07623e32a1a19bd2e164fa301
Wayne657-glitch <flamyy@outlook.com>
2025-01-25 06:54:36 +0000

    Jan 25, 2025, 6:54 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -9,3 +9,13 @@
         #toppings = topping
     #print("Your pizza has the following toppings:" + toppings)
 #print("You have chosen the following toppings: " + toppings)
+questions =  ["How many days is in 1 week?",
+              "How many hours are in 1 day?",
+              "How many minutes are in 1 hour?",
+              "How many seconds are in 1 minute?"]
+for question in questions:
+    answer = input(question)
+    if answer == "end":
+        break
+    print("You did it?")
+
