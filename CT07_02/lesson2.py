c963d94d3faf65e05b5c2ef28acc05874f6b89d8
Wayne657-glitch <flamyy@outlook.com>
2025-01-25 06:56:59 +0000

    Jan 25, 2025, 6:56 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -9,12 +9,6 @@
         #toppings = topping
     #print("Your pizza has the following toppings:" + toppings)
 #print("You have chosen the following toppings: " + toppings)
-questions =  ["How many days is in 1 week?",
-              "How many hours are in 1 day?",
-              "How many minutes are in 1 hour?",
-              "How many seconds are in 1 minute?"]
-for question in questions:
-    if question == "How many days is in 1 week?":
-        print("7")
+
         
 
