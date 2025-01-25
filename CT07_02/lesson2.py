89118fc9eb3072aba2809fcf7ca8706dfb1d2a8a
Wayne657-glitch <flamyy@outlook.com>
2025-01-25 06:51:15 +0000

    Jan 25, 2025, 6:51 AM

--- CT07_02/lesson2.py
+++ CT07_02/lesson2.py
@@ -1,11 +1,11 @@
 toppings = ""
 while True:
-    topping = input("Enter a topping! (type 'end' to finish):")
-    if topping.lower() == 'end':
-        break
-    if toppings:
-        toppings += ", " + topping
-    else:
-        toppings = topping
-    print("Your pizza has the following toppings:" + toppings)
-print("You have chosen the following toppings: " + toppings)
+    #topping = input("Enter a topping! (type 'end' to finish):")
+   # if topping.lower() == 'end':
+       # break
+    #if toppings:
+        #toppings += ", " + topping
+    #else:
+        #toppings = topping
+    #print("Your pizza has the following toppings:" + toppings)
+#print("You have chosen the following toppings: " + toppings)
