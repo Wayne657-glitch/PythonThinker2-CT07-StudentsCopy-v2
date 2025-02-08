060fc4df1e6551a3efe4107e31723460c6a8e2c5
Wayne657-glitch <flamyy@outlook.com>
2025-02-08 07:21:15 +0000

    Feb 8, 2025, 7:21 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -6,24 +6,24 @@
   #  time.sleep(1)
  #   count -= 1
 #print("Happy New Year!")
-planets = [
-    "Mercury",
-    "Venus",
-    "Earth",
-    "Mars",
-    "Jupiter",
-    "Saturn",
-    "Uranus",
-    "Neptune",
-]
-planets.insert(3, "Lalaland")
+#planets = [
+   # "Mercury",
+   # "Venus",
+   # "Earth",
+   # "Mars",
+   # "Jupiter",
+    #"Saturn",
+    #"Uranus",
+    #"Neptune",
+#]
+#planets.insert(3, "Lalaland")
 
-for planet in planets:
+#for planet in planets:
    # if planet == "Earth":
        # print(f"{planet} - This is my home.")
    # elif planet == "Mars":
-        print(f"{planet} - I conquered this.")
+        #print(f"{planet} - I conquered this.")
    # elif planet == "Lalaland":
-        print(f"{planet} - I created this.")
+       # print(f"{planet} - I created this.")
   #  else:
  #       print(planet)
