34430ab2a40728a392043c224272c8699427ea78
Wayne657-glitch <flamyy@outlook.com>
2025-02-08 07:23:11 +0000

    Feb 8, 2025, 7:23 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -27,3 +27,13 @@
        # print(f"{planet} - I created this.")
   #  else:
  #       print(planet)
+countries_to_visit = []
+
+while True:
+    country = input("What country would you like to visit? (type 'done' to finish): ")
+    if country.lower() == 'done':
+        break
+    countries_to_visit.append(country)
+
+for country in countries_to_visit:
+    print(f"I would like to visit {country}")
