23299c29cae7146d0390ce0a274a32be6d16c0b5
Wayne657-glitch <flamyy@outlook.com>
2025-02-08 06:36:40 +0000

    Feb 8, 2025, 6:36 AM

--- CT07_04/lesson4.py
+++ CT07_04/lesson4.py
@@ -1,8 +1,8 @@
 import time
 
-count = 10
-while count > 0:
-    print(count)
-    time.sleep(1)
-    count -= 1
-print("Happy New Year!")
+#count = 10
+#while count > 0:
+ #   print(count)
+  #  time.sleep(1)
+ #   count -= 1
+#print("Happy New Year!")
